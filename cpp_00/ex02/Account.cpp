298c28f83ac56ea2413caf0200c58d91639136b2
Nicosator94 <nicosator94@gmail.com>
2024-04-04 14:25:08 +0200

    cpp00_ex02

---
+++ cpp_00/ex02/Account.cpp
@@ -0,0 +1,64 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Account.cpp                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: niromano <niromano@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/04/04 13:26:36 by niromano          #+#    #+#             */
+/*   Updated: 2024/04/04 14:24:21 by niromano         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Account.hpp"
+
+Account::Account( int initial_deposit )
+{
+	
+}
+Account::~Account( void ) {}
+
+static int	getNbAccounts( void )
+{
+	
+}
+
+static int	getTotalAmount( void )
+{
+	
+}
+
+static int	getNbDeposits( void )
+{
+	
+}
+
+static int	getNbWithdrawals( void )
+{
+	
+}
+
+static void	displayAccountsInfos( void )
+{
+	
+}
+
+void	Account::makeDeposit( int deposit )
+{
+	
+}
+
+bool	Account::makeWithdrawal( int withdrawal )
+{
+	
+}
+
+int		Account::checkAmount( void ) const
+{
+
+}
+
+void	Account::displayStatus( void ) const
+{
+	
+}
