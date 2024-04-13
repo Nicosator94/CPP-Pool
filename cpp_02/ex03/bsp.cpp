cf1231397b99efeb80f1cb6803212773a99b39c9
Nicolas <nicolas.romanoff94@gmail.com>
2024-04-13 16:35:51 +0200

    cpp02_ex03

--- cpp_02/ex03/bsp.cpp
+++ cpp_02/ex03/bsp.cpp
@@ -6,21 +6,21 @@
 /*   By: niromano <niromano@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/12 21:47:29 by niromano          #+#    #+#             */
-/*   Updated: 2024/04/13 02:36:50 by niromano         ###   ########.fr       */
+/*   Updated: 2024/04/13 16:33:08 by niromano         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Fixed.hpp"
 #include "Point.hpp"
 
+Fixed	check_side(Point const p1, Point const p2, Point const point) {
+	return ((p1.getX() - point.getX()) * (p2.getY() - point.getY())) - ((p1.getY() - point.getY()) * (p2.getX() - point.getX()));
+}
+
 bool bsp(Point const a, Point const b, Point const c, Point const point) {
-	if ((((a.getX() - point.getX()) * (b.getY() - point.getY())) - ((a.getY() - point.getY()) * (b.getX() - point.getX()))) > 0 &&
-	(((b.getX() - point.getX()) * (c.getY() - point.getY())) - ((b.getY() - point.getY()) * (c.getX() - point.getX()))) > 0 &&
-	(((c.getX() - point.getX()) * (a.getY() - point.getY())) - ((c.getY() - point.getY()) * (a.getX() - point.getX()))) > 0)
-		return true;
-	else if ((((a.getX() - point.getX()) * (b.getY() - point.getY())) - ((a.getY() - point.getY()) * (b.getX() - point.getX()))) < 0 &&
-	(((b.getX() - point.getX()) * (c.getY() - point.getY())) - ((b.getY() - point.getY()) * (c.getX() - point.getX()))) < 0 &&
-	(((c.getX() - point.getX()) * (a.getY() - point.getY())) - ((c.getY() - point.getY()) * (a.getX() - point.getX()))) < 0)
-		return true;
-	return false;
+	Fixed	d1(check_side(a, b, point));
+	Fixed	d2(check_side(b, c, point));
+	Fixed	d3(check_side(c, a, point));
+	
+	return (d1 > 0 && d2 > 0 && d3 > 0) || (d1 < 0 && d2 < 0 && d3 < 0);
 }
--- cpp_02/ex03/main.cpp
+++ cpp_02/ex03/main.cpp
@@ -6,7 +6,7 @@
 /*   By: niromano <niromano@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/09 13:20:06 by niromano          #+#    #+#             */
-/*   Updated: 2024/04/13 02:50:36 by niromano         ###   ########.fr       */
+/*   Updated: 2024/04/13 16:35:30 by niromano         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,9 +16,9 @@
 
 int main() {
 	Point a(0, 0);
-	Point b(0, 0);
-	Point c(0, 0);
-	Point p(0, 0);
-	std::cout << bsp(a, b, c, p) << std::endl;
+	Point b(5, 5);
+	Point c(10, 0);
+	Point p(5, 3);
+	std::cout << (bsp(a, b, c, p) ? "true" : "false") << std::endl;
 	return 0;
 }
