6c7aed5462de0cf2eff048294f715cccfcf4250b
Nicosator94 <nicosator94@gmail.com>
2024-05-13 11:22:44 +0200

    cpp_05/ex00

--- cpp_05/ex00/main.cpp
+++ cpp_05/ex00/main.cpp
@@ -6,7 +6,7 @@
 /*   By: niromano <niromano@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/23 10:54:08 by niromano          #+#    #+#             */
-/*   Updated: 2024/04/23 15:48:16 by niromano         ###   ########.fr       */
+/*   Updated: 2024/05/13 11:22:32 by niromano         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,9 +15,17 @@
 #include <stdexcept>
 #include "Bureaucrat.hpp"
 
-void	call(void (fn)()) {
+void	call(Bureaucrat *s, char sign) {
 	try {
-		fn();
+		switch (sign) {
+			case '+':
+				s->upGrade();
+				break ;
+			case '-':
+				s->downGrade();
+				break;
+		}
+		
 	} catch (std::exception &e) {
 		std::cout << e.what() << std::endl;
 	}
@@ -25,14 +33,32 @@ void	call(void (fn)()) {
 
 int main() {
 	try {
+		Bureaucrat s("Name", 151);
+		std::cout << "Not supposed to appear" << std::endl;
+	} catch (std::exception &e) {
+		std::cout << e.what() << std::endl;
+	}
+	std::cout << std::endl;
+	try {
+		Bureaucrat s("Name", 0);
+		std::cout << "Not supposed to appear" << std::endl;
+	} catch (std::exception &e) {
+		std::cout << e.what() << std::endl;
+	}
+	std::cout << std::endl;
+	try {
 		Bureaucrat s("Name", 150);
 		std::cout << s.getName() << std::endl;
 		std::cout << s.getGrade() << std::endl;
-		call(s.downGrade());
+		call(&s, '-');
 		std::cout << s.getGrade() << std::endl;
-		call(s.upGrade());
+		call(&s, '+');
 		std::cout << s.getGrade() << std::endl;
+		for (int i = 0; i < 148; i++)
+			call(&s, '+');
 		std::cout << s << std::endl;
+		call(&s, '+');
+		std::cout << s.getGrade() << std::endl;
 	} catch (std::exception &e) {
 		std::cout << e.what() << std::endl;
 	}
