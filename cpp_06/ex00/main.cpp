2dc00bd572e42d95bfb9dca9ed20a4969085d79e
nicolas <nicolas.romanoff94@gmail.com>
2024-05-23 15:56:37 +0200

    cpp06_ex00

--- cpp_06/ex00/main.cpp
+++ cpp_06/ex00/main.cpp
@@ -6,7 +6,7 @@
 /*   By: niromano <niromano@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/21 09:36:00 by niromano          #+#    #+#             */
-/*   Updated: 2024/05/23 15:50:22 by niromano         ###   ########.fr       */
+/*   Updated: 2024/05/23 15:55:00 by niromano         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,9 +19,18 @@ void converter(const std::string &s) {
 	ScalarConverter::convert(s);
 }
 
-int main() {
-	converter("0.1");
-	std::cout << std::endl;
-	converter("0.1f");
+int main(int ac, char **av) {
+	if (ac > 1) {
+		for (int i = 1; i < ac; i++) {
+			converter(av[i]);
+			if (i < ac - 1)
+				std::cout << std::endl;
+		}
+	}
+	else {
+		converter("0.1");
+		std::cout << std::endl;
+		converter("0.1f");
+	}
 	return 0;
 }
