f2e806ebe338c33f8c988a6c5b9ef83059d78adb
nicolas <nicolas.romanoff94@gmail.com>
2024-05-23 17:13:39 +0200

    cpp06_ex01

---
+++ cpp_06/ex01/Serialization.hpp
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Serialization.hpp                                  :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: niromano <niromano@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/05/23 16:47:00 by niromano          #+#    #+#             */
+/*   Updated: 2024/05/23 17:13:28 by niromano         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#pragma once
+
+class Serialization {
+
+	public :
+
+		Serialization();
+		Serialization(const Serialization &copy);
+		Serialization& operator=(const Serialization &serialization);
+		~Serialization();
+
+	private :
+
+};
