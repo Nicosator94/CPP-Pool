b31b198985744d579ef7eb52b45a37c3dd3822bd
Nicosator94 <nicosator94@gmail.com>
2024-08-21 14:05:01 +0200

    Copy constr

--- cpp_08/ex02/MutantStack.hpp
+++ cpp_08/ex02/MutantStack.hpp
@@ -6,7 +6,7 @@
 /*   By: niromano <niromano@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/12 14:22:52 by niromano          #+#    #+#             */
-/*   Updated: 2024/06/12 15:06:07 by niromano         ###   ########.fr       */
+/*   Updated: 2024/08/21 14:02:43 by niromano         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,7 +20,7 @@ template <typename T> class MutantStack : public std::stack<T> {
 
 		MutantStack() {}
 		MutantStack(const MutantStack &copy) : std::stack<T>(copy) {}
-		MutantStack& operator=(const MutantStack &mutantstack) {return *this;}
+		MutantStack& operator=(const MutantStack &mutantstack) {(void)mutantstack; return *this;}
 		~MutantStack() {}
 		typedef typename std::stack<T>::container_type::iterator iterator;
 		iterator begin() {return this->c.begin();}
