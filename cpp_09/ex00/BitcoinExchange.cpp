333ad9ece3657eb8ced70d87276be93575278ca0
Nicosator94 <nicosator94@gmail.com>
2024-08-21 17:44:39 +0200

    bitcoin

--- cpp_09/ex00/BitcoinExchange.cpp
+++ cpp_09/ex00/BitcoinExchange.cpp
@@ -6,11 +6,19 @@
 /*   By: niromano <niromano@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/12 15:14:35 by niromano          #+#    #+#             */
-/*   Updated: 2024/08/21 16:32:07 by niromano         ###   ########.fr       */
+/*   Updated: 2024/08/21 17:44:30 by niromano         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "BitcoinExchange.hpp"
+
+// bool isValidDate(const std::string &date) {
+// 	struct tm timePtr;
+// 	if (strptime(date.c_str(), "%Y-%m-%d", &timePtr) == NULL)
+// 		return false;
+// 	std::cout << timePtr.tm_mday << std::endl;
+// 	return true;
+// }
 
 std::map<std::string, float> getData() {
 	std::ifstream dataFd("data.csv");
@@ -19,16 +27,14 @@ std::map<std::string, float> getData() {
 	std::map<std::string, float> data;
 	std::string buf;
 	std::getline(dataFd, buf, '\n');
-	struct tm timePtr;
 	while (std::getline(dataFd, buf, '\n')) {
 		size_t find = buf.find(",");
 		if (find == std::string::npos) {
 			dataFd.close();
 			throw Error("Missing ',' in \"data.csv\" !");
 		}
-		std::cout << buf.substr(0, find).c_str() << std::endl;
-		strptime(buf.substr(0, find).c_str(), "%Y-%m-%d", &timePtr);
-		std::cout << mktime(&timePtr) << std::endl;
+		// if (!isValidDate(buf.substr(0, find)))
+		// 	std::cout << "pas cool" << std::endl;
 		char *endPtr;
 		data.insert(std::make_pair(buf.substr(0, find), strtof(buf.substr(find + 1, buf.size() - find).c_str(), &endPtr)));
 		std::string end = endPtr;
